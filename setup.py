22a5faf4db30a9aea9122af444f9adcd715ebd47
Frederic98 <frederic.meeuwissen@gmail.com>
2018-05-28 20:05:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ns_api',
+      version='0.1',
+      description='An unofficial package to communicate with the NS API',
+      url='https://github.com/Frederic98/ns_api',
+      author='Frederic98',
+      packages=['ns_api'],
+      zip_safe=False)
